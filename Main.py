71c8c0538e092fd257cd1c94ec5295e49a6879a8
URK21CO3026 <110015042+urk21co3026@users.noreply.github.com>
2022-11-03 22:35:28 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,34 +1,31 @@
 from typing import List
 
-def merge(left_array: List[int], m: int, right_array: List[int], n: int) -> None:
+def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
   # Write code here
-  left_array= left_array[:m]
-  right_array= right_array[:n]
-  data=left_array + right_array
-  i=0
-  j=0
-  k=0
-
-  while i < len(left_array) and j < len(right_array):
-    if left_array[i] <= right_array[j]:
-      data[k] = left_array[i]
-      i += 1
+    array = [0 for i in range(m + n)]
+    i = 0
+    j = 0
+    k = 0
+    while i < m and j < n:
+        if nums1[i] <= nums2[j]:
+            array[k] = nums1[i]
+            i += 1
+        else:
+            array[k] = nums2[j]
+            j += 1
+        k += 1
+    if i >= m:
+        while j < n:
+            array[k] = nums2[j]
+            j += 1
+            k += 1
     else:
-      data[k] = right_array[j]
-      j += 1
-    k += 1
-
-  while i < len(left_array):
-    data[k] = left_array[i]
-    i += 1
-    k += 1
-
-  while j < len(right_array):
-     data[k]=right_array[j]
-     j += 1
-     k += 1
-  print(data)
-
+        while i < m:
+            array[k] = nums1[i]
+            i += 1
+            k += 1
+    for i in range(len(array)):
+      nums1[i] = array[i]
 
 
 # Do not change the following code
@@ -41,3 +38,4 @@ for item in input().split(', '):
 m = int(input())
 n = int(input())
 merge(nums1, m, nums2, n)
+print(nums1)
